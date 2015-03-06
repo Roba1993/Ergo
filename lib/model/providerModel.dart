79768ed5176be763bec1579a992a7fff7945d20e
Rubén García <rgarciau@sistemas.edu.pe>
2015-03-06 12:15:12 +0100

    Improved provider view with html (Description2)
    Ich habe ein Element "description2" hinzugefügt damit wir unter die
    Bottons mehr html hinzufügen können.
    
    Es muss noch die Url´s bearbeiten werden, die der Text zeigt.

--- lib/model/providerModel.dart
+++ lib/model/providerModel.dart
@@ -8,19 +8,20 @@ class ProviderModel {
   String pictureUrl;
   String url;
   String description;
+  String description2;
 
   // the constructor
-  ProviderModel(this.name, this.description, this.pictureUrl, this.url);
+  ProviderModel(this.name, this.description, this.description2, this.pictureUrl, this.url);
 
   // create a json from this object
   Map<String, dynamic> toJson() => <String, dynamic>{
       "name": name,
       "description": description,
+      "description2": description2,
       "pictureUrl": pictureUrl,
       "url": url
   };
 
   // generate a object from json
-  ProviderModel.fromJson(Map<String, dynamic> json): this(json['name'], json['description'], json['pictureUrl'],
-  json['url']);
+  ProviderModel.fromJson(Map<String, dynamic> json): this(json['name'], json['description'], json['description2'], json['pictureUrl'], json['url']);
 }
