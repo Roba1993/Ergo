e66871fbc2a45462a7e5a9ec0d89a97882ac241b
Robert Schütte <schuetterobert@gmail.com>
2015-03-07 14:16:46 +0100

    added insurance information pictures

--- lib/model/providerModel.dart
+++ lib/model/providerModel.dart
@@ -6,20 +6,22 @@ library Ergo.model.providerModel;
 class ProviderModel {
   String name;
   String pictureUrl;
+  String pictureBottomUrl;
   String url;
   String description;
 
   // the constructor
-  ProviderModel(this.name, this.description, this.pictureUrl, this.url);
+  ProviderModel(this.name, this.description, this.pictureUrl, this.pictureBottomUrl, this.url);
 
   // create a json from this object
   Map<String, dynamic> toJson() => <String, dynamic>{
       "name": name,
       "description": description,
       "pictureUrl": pictureUrl,
+      "pictureBottomUrl": pictureBottomUrl,
       "url": url
   };
 
   // generate a object from json
-  ProviderModel.fromJson(Map<String, dynamic> json): this(json['name'], json['description'], json['pictureUrl'], json['url']);
+  ProviderModel.fromJson(Map<String, dynamic> json): this(json['name'], json['description'], json['pictureUrl'], json['pictureBottomUrl'], json['url']);
 }
