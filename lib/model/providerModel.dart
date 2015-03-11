7d6bb01a90b5ed70c96e793c489bc3e29a268b9f
Robert Schütte <schuetterobert@gmail.com>
2015-03-11 19:27:37 +0100

    provider

--- lib/model/providerModel.dart
+++ lib/model/providerModel.dart
@@ -6,22 +6,24 @@ library Ergo.model.providerModel;
 class ProviderModel {
   String name;
   String pictureUrl;
+  String pictureViewUrl;
   String pictureBottomUrl;
   String url;
   String description;
 
   // the constructor
-  ProviderModel(this.name, this.description, this.pictureUrl, this.pictureBottomUrl, this.url);
+  ProviderModel(this.name, this.description, this.pictureUrl, this.pictureViewUrl, this.pictureBottomUrl, this.url);
 
   // create a json from this object
   Map<String, dynamic> toJson() => <String, dynamic>{
       "name": name,
       "description": description,
       "pictureUrl": pictureUrl,
+      "pictureViewUrl": pictureViewUrl,
       "pictureBottomUrl": pictureBottomUrl,
       "url": url
   };
 
   // generate a object from json
-  ProviderModel.fromJson(Map<String, dynamic> json): this(json['name'], json['description'], json['pictureUrl'], json['pictureBottomUrl'], json['url']);
+  ProviderModel.fromJson(Map<String, dynamic> json): this(json['name'], json['description'], json['pictureUrl'], json['pictureViewUrl'], json['pictureBottomUrl'], json['url']);
 }
